8677143d4f3c947549271e932dbf00a86ba2d25f
Vaclav Haken <vaclav@vaclavkabat.net>
2018-05-15 16:41:02 +0100

    adding probe controller

---
+++ src/SonarWebHookFrontAPI/Controllers/ProbeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SonarWebHookFrontAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Probe")]
+    public class ProbeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
